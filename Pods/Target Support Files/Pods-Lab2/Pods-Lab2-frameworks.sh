412ac5881d0ade782d0baff907c8ff308b2f509e
TriNgo <ngominhtrint@gmail.com>
2016-06-27 20:28:19 +0700

    just commit

--- Pods/Target Support Files/Pods-Lab2/Pods-Lab2-frameworks.sh
+++ Pods/Target Support Files/Pods-Lab2/Pods-Lab2-frameworks.sh
@@ -82,3 +82,10 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-Lab2/AFNetworking.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-Lab2/AFNetworking.framework"
+fi

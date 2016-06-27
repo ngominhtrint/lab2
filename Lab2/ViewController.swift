0982de4118650155433fb75f247604ef5b204bb1
TriNgo <ngominhtrint@gmail.com>
2016-06-27 20:47:16 +0700

    load data from network

--- Lab2/ViewController.swift
+++ Lab2/ViewController.swift
@@ -10,14 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var datas = [NSDictionary]?()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        loadDataFromNetwork()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func  loadDataFromNetwork() {
+        let url = NSURL(string: "https://fancy-raptor.hyperdev.space?offset=2&limit=2")
+        
+        let request = NSURLRequest(
+            URL: url!,
+            cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
+            timeoutInterval: 10)
+
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate: nil,
+            delegateQueue: NSOperationQueue.mainQueue()
+        )
+        
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request) { (dataOrNil, response, error) in
+            if let data = dataOrNil {
+                if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                    data, options:[]) as? [NSDictionary] {
+                    
+                    self.datas = responseDictionary
+                    print(self.datas)
+                }
+            }
+        }
+        
+        task.resume()
+        
     }
 }
 
